7ec2928a47c2da73311bed1c4b9ccb385b9ef8b7
smolnik42 <kalyan.smol@mail.ru>
2025-09-15 19:17:41 +0300

    Create install_mac.sh

---
+++ install_mac.sh
@@ -0,0 +1,10 @@
+sudo apt update
+sudo apt install -y python3 python3-pip python3-venv
+sudo apt install snapd
+sudo snap install pycharm-community --classic
+python3 -m venv myenv
+source myenv/bin/activate
+pipx install playwright 
+pipx install pytest 
+python3 -m playwright install chromium
+python 1_test.py
