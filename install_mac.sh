7f7541618e1603d3474653c4010bba175af54c96
smolnik42 <kalyan.smol@mail.ru>
2025-09-15 19:26:00 +0300

    Update install_mac.sh

--- install_mac.sh
+++ install_mac.sh
@@ -1,10 +1,12 @@
-sudo apt update
-sudo apt install -y python3 python3-pip python3-venv
-sudo apt install snapd
-sudo snap install pycharm-community --classic
+
+brew update
+brew install python3
+python3 -m pip install --user pipx
+python3 -m pipx ensurepath
+brew install --cask pycharm-ce
 python3 -m venv myenv
 source myenv/bin/activate
-pipx install playwright 
-pipx install pytest 
+pipx install playwright
+pipx install pytest
 python3 -m playwright install chromium
-python 1_test.py
+python3 1_test.py
